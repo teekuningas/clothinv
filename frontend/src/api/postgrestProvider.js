186c4841a88b8be1c68842fb13a86821b84e062a
teekuningas <erkkahe@gmail.com>
2025-05-04 11:19:37 +0000

    feat: Implement image handling in addItem and listItems functions

--- frontend/src/api/postgrestProvider.js
+++ frontend/src/api/postgrestProvider.js
@@ -451,6 +451,8 @@ export const addItem = async (settings, data) => {
     if (data.imageFile instanceof File) { // Ensure it's a File object
         try {
             const base64Data = await readFileAsBase64(data.imageFile);
+            console.log("addItem");
+            console.log(base64Data);
             imageId = await _insertImage(settings, base64Data, data.imageFile.type, data.imageFile.name);
         } catch (error) {
             console.error("Failed to process or insert image:", error);
@@ -628,6 +630,8 @@ export const listItems = async (settings) => {
                 headers: defaultHeaders(settings, false)
             });
             if (imagesRes.ok) {
+                console.log("After");
+                console.log(imagesRes);
                 const imagesResult = await handleResponse(imagesRes, 'list', 'images');
                 const imagesData = imagesResult.data || [];
                 // 3. Create a map of image_id -> { blob, filename }
